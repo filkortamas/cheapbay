1238a1c9df01312b2af62d683a2131076b4f8305
filkordev <filkordev@protonmail.com>
2019-07-21 00:01:52 +0200

    setup db, make db functions async, linter, prettier

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
+const express = require('express');
 const scraper = require('./lib/scraper');
 const db = require('./lib/db');
-const express = require('express');
+const utility = require('./lib/utility');
+
 const app = express();
-
 const port = 3000;
-
 
 app.get('/scrape', async (req, res) => {
   const html = await scraper.getHtml('thinkpad+x1+6th');
   const scrapedItems = scraper.getItem(html);
-  res.json(scrapedItems);
+
+  const changedItems = await utility.changedItems(scrapedItems);
   
-  scrapedItems.forEach(item => {
-    db.addItemToDb(item)
-  });
+  if(!changedItems) return;
+
+  const dataBase = await db.openDb();
+
+  for (const item of changedItems) {
+    await db.addItemToDb(dataBase, item);
+  }
+
+  await db.closeDb(dataBase);
+
+  res.json(scrapedItems);
 })
+
+app.get('/kecske', async (req, res) => {
+  const dataBase = await db.openDb();
+  await db.selectFromDb(dataBase);
+  res.json('kecske');
+});
 
 app.listen(port, () => console.log(`Cheabay app listening on port ${port}!`))
