9f11ace7da357da4e784e08623463307e09a8ddd
filkordev <filkordev@protonmail.com>
2019-07-22 06:34:14 +0200

    detect changes on db

--- index.js
+++ index.js
@@ -10,20 +10,21 @@ app.get('/scrape', async (req, res) => {
   const html = await scraper.getHtml('thinkpad+x1+6th');
   const scrapedItems = scraper.getItem(html);
 
-  const changedItems = await utility.changedItems(scrapedItems);
-  
-  if(!changedItems) return;
+  const changes = await utility.changes(scrapedItems);
+
+  if (!changes) return;
 
   const dataBase = await db.openDb();
 
-  for (const item of changedItems) {
+  /* for (const item of changes) {
     await db.addItemToDb(dataBase, item);
-  }
+  } */
+  // TODO: make changes on db
 
   await db.closeDb(dataBase);
 
   res.json(scrapedItems);
-})
+});
 
 app.get('/kecske', async (req, res) => {
   const dataBase = await db.openDb();
@@ -31,4 +32,4 @@ app.get('/kecske', async (req, res) => {
   res.json('kecske');
 });
 
-app.listen(port, () => console.log(`Cheabay app listening on port ${port}!`))
+app.listen(port, () => console.log(`Cheabay app listening on port ${port}!`));
